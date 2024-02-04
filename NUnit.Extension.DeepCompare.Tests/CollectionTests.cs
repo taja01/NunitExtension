88453947864c4cb36f38b0d58a4f1d132390c219
taja01 <jarvas.tamas@gmail.com>
2024-02-04 17:56:13 +0100

    adding tests

---
+++ NUnit.Extension.DeepCompare.Tests/CollectionTests.cs
@@ -0,0 +1,121 @@
+﻿using NUnit.Framework;
+
+namespace NUnit.Extension.DeepCompare.Tests
+{
+    [TestFixture]
+    public class CollectionTests
+    {
+        [Test]
+        public void DifferentCollectionTest()
+        {
+            var arr = new[] { new object() };
+            var list = new List<object>();
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(arr, Is.DeeplyEqualTo(list)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'Different Type: ' mismatch: Expected 'List`1', but was 'Object[]'."));
+        }
+
+        [Test]
+        public void StringCollectionTest()
+        {
+            var expectedList = new List<string?> { "1", "2", null };
+            var actualList = new List<string?> { "1", "2", null };
+
+            Assert.That(expectedList, Is.DeeplyEqualTo(actualList));
+        }
+
+        [Test]
+        public void StringCollectionTest2()
+        {
+            var expectedList = new List<string> { "1", "2" };
+            var actualList = new List<string> { "1", "2" };
+
+            Assert.That(expectedList, Is.DeeplyEqualTo(actualList));
+        }
+
+        [Test]
+        public void StringCollectionTest3()
+        {
+            var expectedList = new List<string?> { "1", "2", null };
+            var actualList = new List<string?> { "1", "2", "" };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(expectedList, Is.DeeplyEqualTo(actualList)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'System.Reflection.PropertyInfo[].[2].' mismatch: Expected 'Empty', but was 'null'"));
+        }
+
+        [Test]
+        public void StringCollectionTest4()
+        {
+            var expectedList = new List<string?> { "1", "2", "" };
+            var actualList = new List<string?> { "1", "2", null };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(expectedList, Is.DeeplyEqualTo(actualList)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'System.Reflection.PropertyInfo[].[2].' mismatch: Expected 'null', but was 'Empty'"));
+        }
+
+        [Test]
+        public void StringCollectionDDifferentLenghtTest()
+        {
+            var expectedList = new List<string?> { "1", "2", null, "4" };
+            var actualList = new List<string?> { "1", "2", null };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(expectedList, Is.DeeplyEqualTo(actualList)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'System.Reflection.PropertyInfo[].Count' mismatch: Expected 'Count 3', but was 'Count 4'."));
+        }
+
+        [Test]
+        public void IntCollectionTest()
+        {
+            var expectedList = new List<int?> { 1, 2, 3, null };
+            var actualList = new List<int?> { 1, 2, 3, null };
+
+            Assert.That(expectedList, Is.DeeplyEqualTo(actualList));
+        }
+
+        [Test]
+        public void IntCollectionTest2()
+        {
+            var expectedList = new List<int?> { 1, 2, 3, null };
+            var actualList = new List<int?> { 1, 2, 3, 4 };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(expectedList, Is.DeeplyEqualTo(actualList)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'System.Reflection.PropertyInfo[].[3].' mismatch: Expected '4', but was 'null'."));
+        }
+
+        [Test]
+        public void IntCollectionTest3()
+        {
+            var expectedList = new List<int?> { 1, 2, 3, 4 };
+            var actualList = new List<int?> { 1, 2, 3, null };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(expectedList, Is.DeeplyEqualTo(actualList)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'System.Reflection.PropertyInfo[].[3].' mismatch: Expected 'null', but was '4'."));
+        }
+
+        [Test]
+        public void CharCollectionTest()
+        {
+            var expectedList = new List<char?> { 'a', '1', ' ' };
+            var actualList = new List<char?> { 'a', '1', ' ' };
+
+            Assert.That(expectedList, Is.DeeplyEqualTo(actualList));
+        }
+
+        [Test]
+        public void CharCollectionTest2()
+        {
+            var expectedList = new List<char?> { 'a', '1', ' ', '+' };
+            var actualList = new List<char?> { 'a', '1', ' ', '-' };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(expectedList, Is.DeeplyEqualTo(actualList)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'System.Reflection.PropertyInfo[].[3].' mismatch: Expected '-', but was '+'."));
+        }
+    }
+}
---
+++ NUnit.Extension.DeepCompare.Tests/RequestBodyTests.cs
@@ -0,0 +1,138 @@
+﻿using NUnit.Framework;
+
+namespace NUnit.Extension.DeepCompare.Tests
+{
+    [TestFixture]
+    public class RequestBodyTests
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            var actual = new ResponseBody();
+            var expected = new ResponseBody();
+
+            Assert.That(actual, Is.DeeplyEqualTo(expected));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var actual = new ResponseBody { IsSuccess = true };
+            var expected = new ResponseBody { IsSuccess = true };
+
+            Assert.That(actual, Is.DeeplyEqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var actual = new ResponseBody { IsSuccess = true };
+            var expected = new ResponseBody { IsSuccess = false };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(actual, Is.DeeplyEqualTo(expected)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'IsSuccess' mismatch: Expected 'False', but was 'True'."));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var actual = new ResponseBody { IsSuccess = true, Strings = new List<string> { "22", "44" } };
+            var expected = new ResponseBody { IsSuccess = true, Strings = new List<string> { "22", "34" } };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(actual, Is.DeeplyEqualTo(expected)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'Strings.[1].Chars' mismatch: Expected '34', but was '44'."));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var actual = new ResponseBody { IsSuccess = true, Numbers = new List<int> { 1, 2, 3 } };
+            var expected = new ResponseBody { IsSuccess = true, Numbers = new List<int> { 3, 2, 1 } };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(actual, Is.DeeplyEqualTo(expected)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'Numbers.[0].' mismatch: Expected '3', but was '1'."));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var actual = new ResponseBody
+            {
+                StatusCode = 200,
+                IsSuccess = true,
+                Numbers = new List<int> { 1, 2, 3 },
+                Method = Method.GET
+            };
+            var expected = new ResponseBody
+            {
+                StatusCode = 200,
+                IsSuccess = true,
+                Numbers = new List<int> { 1, 2, 3 },
+            };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(actual, Is.DeeplyEqualTo(expected)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'Method' mismatch: Expected 'null', but was 'GET'."));
+        }
+
+        [Test]
+        public void Test6()
+        {
+            var actual = new ResponseBody
+            {
+                StatusCode = 200,
+                IsSuccess = true,
+                Numbers = new List<int> { 1, 2, 3 },
+                Method = Method.GET,
+                InnerMessage = new InnerMessage { Message = "Done" }
+            };
+            var expected = new ResponseBody
+            {
+                StatusCode = 200,
+                IsSuccess = true,
+                Numbers = new List<int> { 1, 2, 3 },
+                Method = Method.GET,
+                InnerMessage = new InnerMessage { Message = "Waiting" }
+            };
+
+            var ex = Assert.Throws<AssertionException>(() => Assert.That(actual, Is.DeeplyEqualTo(expected)));
+
+            Assert.That(ex.Message, Does.Contain("Property 'InnerMessage.Message' mismatch: Expected 'Waiting', but was 'Done'."));
+        }
+
+        private class ResponseBody
+        {
+            public int StatusCode { get; set; }
+
+            public bool IsSuccess { get; set; }
+
+            public string Message { get; set; }
+
+            public List<int> Numbers { get; set; }
+
+            public List<string> Strings { get; set; }
+
+            public Method? Method { get; set; }
+
+            public InnerMessage InnerMessage { get; set; }
+
+        }
+
+        private class InnerMessage
+        {
+            public string Message { get; set; }
+        }
+
+        enum Method
+        {
+            None,
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
